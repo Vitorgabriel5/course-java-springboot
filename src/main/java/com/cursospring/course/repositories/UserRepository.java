783f2fd936d8f7b03388472c2f0f22d5eb897558
Vitor Gabriel <156606648+Vitorgabriel5@users.noreply.github.com>
2025-11-04 19:49:06 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/cursospring/course/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.cursospring.course.repositories;
+
+import com.cursospring.course.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+}
