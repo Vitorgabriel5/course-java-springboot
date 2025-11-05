c9546348df5fc11f3fb8b55be881f574639e7840
Vitor Gabriel <156606648+Vitorgabriel5@users.noreply.github.com>
2025-11-05 03:42:49 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/cursospring/course/entities/User.java
+++ src/main/java/com/cursospring/course/entities/User.java
@@ -4,10 +4,12 @@ import jakarta.persistence.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
-@Table(name = "tb-user")
+@Table(name = "tb_user")
 public class User implements Serializable {
 
 
@@ -21,6 +23,11 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+
+
 
     private User(){
     }
@@ -71,6 +78,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
