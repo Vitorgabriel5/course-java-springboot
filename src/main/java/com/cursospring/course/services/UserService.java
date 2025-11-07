e050ebf534892b5e4c36e31ac1a4fa48e942101f
Vitor Gabriel <156606648+Vitorgabriel5@users.noreply.github.com>
2025-11-07 15:05:37 -0300

    Exception handling - findById

--- src/main/java/com/cursospring/course/services/UserService.java
+++ src/main/java/com/cursospring/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.cursospring.course.services;
 
 import com.cursospring.course.entities.User;
 import com.cursospring.course.repositories.UserRepository;
+import com.cursospring.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(()-> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
---
+++ src/main/java/com/cursospring/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,16 @@
+package com.cursospring.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(Object id){
+        super("Resource not found. Id: " + id);
+    }
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
+
+
+
+}
